f6602ea67672aa21b0708f5b31a810f4a2a065de
Robertas <robertas.treb@gmail.com>
2020-01-03 18:52:53 +0200

    idejau teksta

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
-app.get("/", (req,res)=>{res.send(database.users)});
+app.get("/", (req,res)=>{res.send("database.users")});
 
 app.post("/signin",(req,res)=>{signIn.handleSignin(req,res,serverPostgres,bcrypt)});
 
